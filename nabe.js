8b72443b219925982eb7cc821912f192288a0189
清遠翔太 <shota@shinenshoutanoMacBook-Air.local>
2020-02-25 22:38:47 +0900

    指摘対応しました。

--- nabe.js
+++ nabe.js
@@ -1,17 +1,12 @@
 
-const a = /3/;
 for (let number = 1; number <= 100; number++) {
-
     if (number % 3 === 0) {
         console.log(number + "!");
-
     }
-    else  if(/3/.test(number.toString())){
+    else if (/3/.test(number.toString())) {
         console.log(number + "!");
-
-
     }
-    else{
+    else {
         console.log(number);
     }
 };
