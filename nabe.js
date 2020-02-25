ae6967858f854c2e0bb43208e36ab91faa29e7bb
清遠翔太 <shota@shinenshoutanoMacBook-Air.local>
2020-02-25 15:09:29 +0900

    作ってあったやつ

---
+++ nabe.js
@@ -0,0 +1,17 @@
+
+const a = /3/;
+for (let number = 1; number <= 100; number++) {
+
+    if (number % 3 === 0) {
+        console.log(number + "!");
+
+    }
+    else  if(/3/.test(number.toString())){
+        console.log(number + "!");
+
+
+    }
+    else{
+        console.log(number);
+    }
+};
